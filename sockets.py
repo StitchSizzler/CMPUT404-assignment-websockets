6a55e27488d45a648abcd7209e5b46397ae42914
StitchSizzler <eisha@ualberta.ca>
2022-04-01 12:44:49 -0600

    first comit

--- sockets.py
+++ sockets.py
@@ -14,76 +14,99 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, Response
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
 import time
 import json
 import os
-
+  
 app = Flask(__name__)
 sockets = Sockets(app)
-app.debug = True
-
+app.debug = True  
+clients = []  
+ 
 class World:
     def __init__(self):
         self.clear()
         # we've got listeners now!
         self.listeners = list()
-        
+       
     def add_set_listener(self, listener):
         self.listeners.append( listener )
-
+ 
     def update(self, entity, key, value):
         entry = self.space.get(entity,dict())
         entry[key] = value
         self.space[entity] = entry
         self.update_listeners( entity )
-
+ 
     def set(self, entity, data):
         self.space[entity] = data
         self.update_listeners( entity )
-
+ 
     def update_listeners(self, entity):
         '''update the set listeners'''
         for listener in self.listeners:
             listener(entity, self.get(entity))
-
+ 
     def clear(self):
         self.space = dict()
-
+ 
     def get(self, entity):
         return self.space.get(entity,dict())
-    
+   
     def world(self):
         return self.space
-
-myWorld = World()        
+  
+myWorld = World()    
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
-
+    msg = json.dumps({entity: data})
+    for client in clients:
+        client.put_nowait(msg)
+ 
 myWorld.add_set_listener( set_listener )
-        
+       
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
-
+    return redirect("/static/index.html")
+ 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
+    while True:
+        msg = ws.receive()
+        if msg is not None:
+            packet = json.loads(msg)
+            for key, value in packet.items():
+                myWorld.set(key, value)
+        else:
+            break
     return None
-
+ 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
-
-
+    client = queue.Queue()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client)
+    try:
+        while True:
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        pass
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+ 
+ 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
 def flask_post_json():
@@ -95,30 +118,37 @@ def flask_post_json():
         return json.loads(request.data.decode("utf8"))
     else:
         return json.loads(request.form.keys()[0])
-
+ 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
-
+    req = flask_post_json()
+    myWorld.set(entity, req)
+    res = myWorld.get(entity)
+    return flask.jsonify(res)
+ 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
-
+    res = myWorld.world()
+    return flask.jsonify(res)
+ 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
-
-
+    res = myWorld.get(entity)
+    return flask.jsonify(res)
+ 
+ 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
-
-
-
+    myWorld.clear()
+    res = myWorld.world()
+    return flask.jsonify(res)
+ 
+ 
+ 
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
         pip install gunicorn
